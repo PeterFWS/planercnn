eb8930a1095ac99e30aa1b41e76f9a7c5a3afc19
PeterFWS <sfw811@hotmail.com>
2020-06-03 21:05:08 +0200

    re-format

--- options.py
+++ options.py
@@ -6,6 +6,7 @@ Licensed under the CC BY-NC-SA 4.0 license
 
 import argparse
 
+
 def parse_args():
     """
     Parse input arguments
@@ -13,7 +14,7 @@ def parse_args():
     parser = argparse.ArgumentParser(description='CornerNet')
     parser.add_argument('--gpu', dest='gpu',
                         help='gpu',
-                        default=1, type=int)    
+                        default=1, type=int)
     parser.add_argument('--task', dest='task',
                         help='task type: [train, test, predict]',
                         default='train', type=str)
@@ -34,13 +35,13 @@ def parse_args():
                         default='../../Data/ScanNet/', type=str)
     parser.add_argument('--anchorFolder', dest='anchorFolder',
                         help='anchor folder',
-                        default='anchors/', type=str)    
+                        default='anchors/', type=str)
     parser.add_argument('--customDataFolder', dest='customDataFolder',
                         help='data folder',
                         default='test/custom', type=str)
     parser.add_argument('--MaskRCNNPath', dest='MaskRCNNPath',
                         help='path to Mask R-CNN weights',
-                        default='../mask_rcnn_coco.pth', type=str)    
+                        default='../mask_rcnn_coco.pth', type=str)
     parser.add_argument('--numTrainingImages', dest='numTrainingImages',
                         help='the number of images to train',
                         default=1000, type=int)
@@ -86,10 +87,10 @@ def parse_args():
     parser.add_argument('--numInputChannels', dest='numInputChannels',
                         help='the number of classes',
                         default=4, type=int)
-    ## Flags
+    # Flags
     parser.add_argument('--visualizeMode', dest='visualizeMode',
                         help='visualization mode',
-                        default='', type=str)    
+                        default='', type=str)
     parser.add_argument('--trainingMode', dest='trainingMode',
                         help='training mode',
                         default='all', type=str)
@@ -104,8 +105,8 @@ def parse_args():
                         default='', type=str)
     parser.add_argument('--blocks', dest='blocks',
                         help='blocks',
-                        default='', type=str)        
-    ## Synthetic dataset
+                        default='', type=str)
+    # Synthetic dataset
     parser.add_argument('--locationNoise', dest='locationNoise',
                         help='the location noise',
                         default=0.0, type=float)
@@ -115,7 +116,7 @@ def parse_args():
     parser.add_argument('--occlusionNoise', dest='occlusionNoise',
                         help='occlusion noise',
                         default=0, type=int)
-    ## Corner net options
+    # Corner net options
     parser.add_argument('--considerPartial', dest='considerPartial',
                         help='consider partial input',
                         action='store_true')
@@ -134,10 +135,10 @@ def parse_args():
     parser.add_argument('--minNumPointRatio', dest='minNumPointRatio',
                         help='the minimum number of points (ratio)',
                         default=0.05, type=float)
-    ## Mask RCNN options
+    # Mask RCNN options
     parser.add_argument('--maskWidth', dest='maskWidth',
                         help='mask width',
-                        default=56, type=int)    
+                        default=56, type=int)
     parser.add_argument('--maskHeight', dest='maskHeight',
                         help='mask height',
                         default=56, type=int)
@@ -147,7 +148,7 @@ def parse_args():
     parser.add_argument('--numPositiveExamples', dest='numPositiveExamples',
                         help='the nummber of positive examples',
                         default=200, type=int)
-    ## Plane options
+    # Plane options
     parser.add_argument('--numAnchorPlanes', dest='numAnchorPlanes',
                         help='the number of anchor planes',
                         default=0, type=int)
@@ -163,10 +164,10 @@ def parse_args():
     parser.add_argument('--scaleMode', dest='scaleMode',
                         help='scale mode',
                         default='variant', type=str)
-    ## Refinement options
+    # Refinement options
     parser.add_argument('--cornerPositiveWeight', dest='cornerPositiveWeight',
                         help='larger weight for corners to fight the positive-negative balance issue',
-                        default=0, type=int)    
+                        default=0, type=int)
     parser.add_argument('--positiveWeight', dest='positiveWeight',
                         help='positive weight',
                         default=0.33, type=float)
@@ -175,14 +176,14 @@ def parse_args():
                         default=1, type=int)
     parser.add_argument('--warpingWeight', dest='warpingWeight',
                         help='warping weight',
-                        default=0.1, type=float)    
+                        default=0.1, type=float)
     parser.add_argument('--convType', dest='convType',
                         help='convolution type',
                         default='2', type=str)
-    ## Evaluation options
+    # Evaluation options
     parser.add_argument('--methods', dest='methods',
                         help='evaluation methods',
                         default='b', type=str)
-    
+
     args = parser.parse_args()
     return args
